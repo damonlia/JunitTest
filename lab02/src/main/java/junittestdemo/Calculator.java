ad79a3f3850697aa6c45ae5ff1405717da0a9043
Natalia Debska <natadebska@hotmail.com>
2016-03-08 10:57:43 +0100

    lab01 w mavenie

--- lab02/src/main/java/junittestdemo/Calculator.java
+++ lab02/src/main/java/junittestdemo/Calculator.java
@@ -1,11 +1,24 @@
 package junittestdemo;
 
 public class Calculator {
-	public int add(int a , int b){
+	public int add(int a, int b){
 		return a+b;
 	}
 	
-	public int mul(int a , int b){
-			return a*b;
+	public int sub(int a, int b){
+		return a-b;
+	}
+	public int multi(int a, int b){
+		return a*b;
+	}
+	public int div(int a, int b){
+		return a/b;
+	}
+	public boolean greater(int a, int b){
+		if(a>b){
+			return true;
 		}
+		else return false;
+		
+	}
 }
--- lab02/src/test/java/junittestdemo/CalculatorTest.java
+++ lab02/src/test/java/junittestdemo/CalculatorTest.java
@@ -1,8 +1,12 @@
 package junittestdemo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 
 
 
@@ -16,8 +20,37 @@ public class CalculatorTest {
 		}
 	 
 	 @Test
+		public void substr(){
+			assertEquals(1,c.sub(5, 4));
+			System.out.println("Sub:" + c);
+		}
+	 
+	 @Test
 		public void multi(){
-			assertEquals(9,c.mul(3, 3));
+			assertEquals(9,c.multi(3, 3));
 			System.out.println("mul:" + c);
+		}
+	 
+	 @Test
+		public void div(){
+			assertEquals(1,c.div(2, 2));
+			System.out.println("div:" + c);
+		}
+	 
+	 @Test
+		public void great(){
+			assertTrue(c.greater(3, 2));
+			System.out.println("greater:" + c);
+		}
+	 
+	 @Rule
+	 public ExpectedException thrown= ExpectedException.none();
+	 
+	 @Test
+		public void divZero(){
+		 System.out.println("przed");
+		 thrown.expect(ArithmeticException.class);
+		 	c.div(1, 0);
+
 		}
 }
