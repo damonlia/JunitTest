6dc43ea1b91f69e5b2ca55c66a273f69f8d27fb4
Natalia Debska <natadebska@hotmail.com>
2016-03-22 08:45:31 +0100

    wej 01

--- wej01/src/main/java/wej01/IRepository.java
+++ wej01/src/main/java/wej01/IRepository.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 
 public interface IRepository {
 	public void addP(Phone p);
+	public void setPhonesAll(ArrayList<Phone> phonesAll);
+	public Phone getPhoneName(String name);
 	
 }
